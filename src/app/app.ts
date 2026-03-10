b58c6f4573e4ce228f3ba8a70ee9dd8ab827723d
Chandan Kumar Nayak <chandankumar.n@dhanushsoftech.com>
2026-03-10 19:04:01 +0530

    Initial commit: KNCCI QR landing page

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LandingPageComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class AppComponent {
+}
